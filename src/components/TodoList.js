6f69dd7e982ff49b2d7bb3e1e575f4c902f94f88
akremdev <akremdev@gmail.com>
2021-04-24 16:47:10 +0100

    useEffect Hooks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AddNewTodo from './AddNewTodo';
 
 const TodoList = () => {
@@ -7,12 +7,20 @@ const TodoList = () => {
          {text : 'Do Your homework', id: 2},
          {text : 'Feed the dog', id: 3},
      ]);
+
+     const [count, setCount] = useState(0);
     const addTodo = (text) => {
         setTodos([
             ...todos, //get all the todos previous ones.
             {text,  id: Math.random()}
         ])
-    }   
+    }
+    useEffect(() => {
+        console.log('use effec', todos);
+    }, [todos]);
+    useEffect(() => {
+        console.log('use effect', count);
+    }, [count]); 
     return (
         <div>
             <ul>
@@ -30,6 +38,7 @@ const TodoList = () => {
                 }
             </ul>
             <AddNewTodo addTodo={addTodo} />
+            <button onClick={() => setCount(count + 1 )}>Score: {count}</button>
         </div>
     )
 }
